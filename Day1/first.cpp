128b90f3a327f3b1d1d4cbc8415d6a5d7d5d847a
Amit-Priyu <priyuocta@gmail.com>
2024-09-11 22:24:42 +0530

    day 1 commit

---
+++ Day1/first.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using namespace std;
+
+
+// hll-> compiler-> ( binary output file)-> desired output.
+// massage:- ""
+int main(){
+    cout<<"hellow world";
+    cout<<"\n";
+}
